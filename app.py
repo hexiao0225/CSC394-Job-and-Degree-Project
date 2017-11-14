935ec4ba9e829fce53096a7617f2d7b30cb54167
culmer <ulmer.colin@gmail.com>
2017-11-14 14:33:48 -0600

    Added API step 1 and 2

--- app.py
+++ app.py
@@ -5,76 +5,108 @@ from flask import Flask, current_app, render_template, request, jsonify
 from config import app
 from models import db, Keyword, Job, Degree, JobKeywords, DegreeKeywords, KeywordAnswer, KeywordQuestion
 
+@app.route('/', methods=['GET'])
+def index():
+    """
+    Return Home Page
+    """
+    return render_template('full.html')
 
-def dbJSON(obj):
+#STEP ONE
+@app.route('/api/titles', methods=['GET'])
+def getTitles():
     """
-    Returns JSON format of database objects
-    """
-    json_obj = {'data': []}
-    for entry in obj:
-        tmp = {}
-        for k,v in entry.__dict__.iteritems():
-            if k != '_sa_instance_state': tmp[k] = v
-        json_obj['data'].append(tmp)
-    return jsonify(json_obj)
+    Return JSON list of titles for Jobs or Degrees based on target parameter in URL
 
+    Job titles
+    /api/titles?target=job
 
+    Degree titles
+    /api/titles?target=degree
     """
-        hard code the first page with degree titles for now.
-    """
-@app.route('/api/degreetitles',methods=['GET'])
-def getDegreeTitles():
-    degrees = Degree.query.all()
-    resp = {'degreetitles': []}
-    for d in degrees:
-        resp['degreetitles'].append(d.title)
+    resp = []
+    target = request.args.get('target') # 'job' or 'degree'
+    if target == 'job':
+        degrees = Degree.query.all()
+        for d in degrees:
+            resp.append(d.title)
+    elif target == 'degree':
+        jobs = Job.query.all()
+        for j in jobs:
+            resp.append(j.title)
     return jsonify(resp)
 
-@app.route('/api/jobtitles', methods=['GET'])
-def getJobTitles():
-    jobs = Job.query.all()
-    resp = {'jobtitles': []}
-    for j in jobs:
-        resp['jobtitles'].append(j.title)
-    return jsonify(resp)
-
+# STEP TWO
 @app.route('/api/skills', methods=['GET'])
-def retrieveSkills():
+def getSkills():
     """
     Retrieves all keywords based on target and title parameter
         i.e /api/skills?target=job&title=software%20developer
     Returns all keywords if no target and title is present
     """
-    resp = {}
+    resp = []
     target = request.args.get('target')
     title = request.args.get('title')
-    if target == None and title == None:
+    if target == None and title == None: #if no input given in parameters
         resp['skills'] = []
         all_skills = Keyword.query.all()
         for s in all_skills:
-            resp['skills'].append(s.key)
+            resp.append(s.key)
         return jsonify(resp)
     if target == 'job' and title != None:
         job_object = Job.query.filter_by(title=title).first()
         if job_object:
-            resp[job_object.title] = []
             keys = JobKeywords.query.filter_by(jobID = job_object.id)
             for k in keys:
                 tmp_key_obj = Keyword.query.get(k.keywordID)
-                resp[job_object.title].append(tmp_key_obj.key)
+                resp.append(tmp_key_obj.key)
             return jsonify(resp)
         return jsonify("Bad Request - title not found %s" % title)
     if target == 'degree' and title != None:
         degree_object = Degree.query.filter_by(title=title).first()
         if degree_object:
-            resp[degree_object.title] = []
             keys = DegreeKeywords.query.filter_by(degreeID = degree_object.id)
             for k in keys:
                 tmp_key_obj = Keyword.query.get(k.keywordID)
-                resp[degree_object.title].append(tmp_key_obj.key)
+                resp.append(tmp_key_obj.key)
             return jsonify(resp)
         return jsonify("Bad Request - title not found")
     return jsonify("Bad Request")
+
+# STEP THREE
+@app.route('/api-new/questions', methods=['GET'])
+def getQuestionsNEW():
+    """
+    GET questions
+    """
+    skills = request.args.get('skills') # skills here should be an array of skills
+
+    # implementation
+
+    return jsonify(
+        [
+            {
+                'id': 'some id for internal identification purpose',
+                'Question':'Question 1 Text',
+                'Options': ['Option 1','Option 2','Option 3','Option 4']
+            },
+            {
+                'id': 'some id for internal identification purpose',
+                'Question':'Question 2 Text',
+                'Options': ['Option 1','Option 2','Option 3','Option 4']
+            },
+            {
+                'id': 'some id for internal identification purpose',
+                'Question':'Question 3 Text',
+                'Options': ['Option 1','Option 2','Option 3','Option 4']
+            },
+            {
+                'id': 'some id for internal identification purpose',
+                'Question':'Question 4 Text',
+                'Options': ['Option 1','Option 2','Option 3','Option 4']
+            }
+        ]
+    )
 
 @app.route('/api/question', methods=['GET'])
 def retrieveQuestion():
@@ -102,36 +134,10 @@ def retrieveQuestion():
     data['options'] = options
     data['answer'] = answer
     return jsonify(data)
-    #return jsonify({'status':200, 'data': data})
 
-@app.route('/', methods=['GET'])
-def index():
-    """
-    Home Page / Survey View
-    """
-    return render_template('full.html')
 
-@app.route('/about', methods=['GET'])
-def about():
-    """
-    About Page view
-    """
-    return render_template('about.html')
 
-@app.route('/calculate-results', methods=['POST'])
-def calculateResults():
-    """
-    POST-only URL that retrieves survey results
-    based on POSTed keywords
-    """
-    pass
 
-@app.route('/email-results', methods=['POST'])
-def emailResults():
-    """
-    POST-only URL for emailing survey results to user
-    """
-    pass
 
 #For testing only, production will need a different server
 if __name__ == '__main__':
