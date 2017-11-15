9ee310ebc3d04091782c28e574beabd7ece51bc9
culmer <ulmer.colin@gmail.com>
2017-11-14 18:04:11 -0600

    Added global config for email

--- config.py
+++ config.py
@@ -12,3 +12,6 @@ from flask import Flask
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kzffhwvbxkqwdx:aa9ce97f6dc72871206b5a8d92805f311f73aac71d604f4402d7ddf97b735046@ec2-50-17-203-195.compute-1.amazonaws.com:5432/d2d0gj6oiffiu2'
 app.config['DEBUG'] = False
+
+app.config['EMAIL'] = ""
+app.config['EMAIL_PASSWORD'] = ""
--- email.py
+++ email.py
@@ -1,10 +1,14 @@
 import smtplib
+import os
+
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 from email.header import Header
-import os
-def sendMailText(receiver, content, images):
+
+from config import app
+
+def sendMailText(receiver, content, images=None):
     msg = MIMEMultipart('related')
     msg['Subject'] = Header("Final Result", "utf-8")
     msg['From'] = "username"
@@ -14,12 +18,13 @@ def sendMailText(receiver, content, images):
     msg.attach(msgAlternative)
     msgAlternative.attach(MIMEText(content, 'html', 'utf-8'))
 
-    fp = open(images, 'rb')
-    msgImage = MIMEImage(fp.read())
-    fp.close()
-    msgImage.add_header('Content-ID', '<0>')
-    msg.attach(msgImage)
+    if images:
+        fp = open(images, 'rb')
+        msgImage = MIMEImage(fp.read())
+        fp.close()
+        msgImage.add_header('Content-ID', '<0>')
+        msg.attach(msgImage)
 
     s = smtplib.SMTP( "host_server", "25")
-    s.login("username", "password" )
-    s.sendmail("username", receiver, msg.as_string())
+    s.login(app.config['EMAIL'], app.config['EMAIL_PASSWORD'] )
+    s.sendmail(app.config['EMAIL'], receiver, msg.as_string())
