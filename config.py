f802a948af8e782cb49ad735dce44afc739a5a46
dejiang27 <32689586+dejiang27@users.noreply.github.com>
2017-11-14 18:42:29 -0600

    Update config.py

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kzffhwvbxkqwdx:aa9ce97f6dc72871206b5a8d92805f311f73aac71d604f4402d7ddf97b735046@ec2-50-17-203-195.compute-1.amazonaws.com:5432/d2d0gj6oiffiu2'
 app.config['DEBUG'] = False
 
-app.config['EMAIL'] = ""
-app.config['EMAIL_PASSWORD'] = ""
+app.config['EMAIL'] = "teamone@writeme.com"
+app.config['EMAIL_PASSWORD'] = "Teamone1234."
